0544788835b685eaf80853c052be37a7caf95560
Tran Hoang Huy <huybeos2707@gmail.com>
2021-02-18 22:48:57 +0700

    using localStorage

--- javascript/api.js
+++ javascript/api.js
@@ -1,49 +1,76 @@
+function saveForm (){
+    var NguoiTao = document.getElementById('NguoiTao').value
+    var NguoiXuLy = document.getElementById('NguoiXuLY').value
+    var TheoKeHoachThang = document.getElementById('TheoKeHoachThang').value
+    var SoTienDuTru = document.getElementById('SoTienDuTru').value
+    var TieuDe = document.getElementById('TieuDe').value
+    var PhongBanLienQuan = document.getElementById('PhongBanLienQuan').value
+    var ThoiGianXuLy = document.getElementById('ThoiGianXuLy').value
+    var ThoiGianXuLy = document.getElementById('PhoiHopView').value
+    var BoPhan = document.getElementById('BoPhan').value
+    var TrangThai = document.getElementById('TrangThai').value
+    var Nam = document.getElementById('Nam').value
+    var ThoiGianBanGiao = document.getElementById('ThoiGianBanGiao').value
+    var NoiDungDeXuat = document.getElementById('NoiDungDeXuat').value
+    var NgayTao = document.getElementById('NgayTao').value
+    var NgayGui = document.getElementById('NgayGui').value
+    var Thuoc = document.getElementById('Thuoc').value
+    var NhaCungCap = document.getElementById('NhaCungCap').value
+    var YKienTroLy = document.getElementById('YKienTroLy').value
+    var MuaSap = document.getElementById('MuaSap').value
+    var DanhSachPO = document.getElementById('DanhSachPO').value
+    // var FileDinhKem = document.getElementById('FileDinhKem').value
+    var a = []
 
-fetch('http://localhost:3000/table').then(res => res.json())
-.then(json => {
-    console.log(json[0].Nam)
-    document.getElementById('NguoiTao').innerHTML = json[0].NguoiTao
-    document.getElementById('NguoiXuLY').innerHTML = json[0].NguoiXuLY
-    document.getElementById('TheoKeHoachThang').innerHTML = json[0].TheoKeHoachThang
-    document.getElementById('SoTienDuTru').innerHTML = json[0].SoTienDuTru
-    document.getElementById('TieuDe').innerHTML = json[0].TieuDe
-    document.getElementById('PhongBanLienQuan').innerHTML = json[0].PhongBanLienQuan
-    document.getElementById('ThoiGianXuLy').innerHTML = json[0].ThoiGianXuLy
-    document.getElementById('PhoiHopView').innerHTML = json[0].PhoiHopView
-    document.getElementById('BoPhan').innerHTML = json[0].BoPhan
-    document.getElementById('TrangThai').innerHTML = json[0].TrangThai
-    document.getElementById('Nam').innerHTML = json[0].Nam
-    document.getElementById('ThoiGianBanGiao').innerHTML = json[0].ThoiGianBanGiao
-    document.getElementById('NoiDungDeXuat').innerHTML = json[0].NoiDungDeXuat
-    document.getElementById('NgayTao').innerHTML = json[0].NgayTao
-    document.getElementById('NgayGui').innerHTML = json[0].NgayGui
-    document.getElementById('Thuoc').innerHTML = json[0].Thuoc
-    document.getElementById('NhaCungCap').innerHTML = json[0].NhaCungCap
-    document.getElementById('YKienTroLy').innerHTML = json[0].YKienTroLy
-    document.getElementById('MuaSap').innerHTML = json[0].MuaSap
-    // document.getElementById('DanhSachPO').innerHTML = json[0].DanhSachPO
-    // document.getElementById('FileDinhKem').innerHTML = json[0].FileDinhKem
-})
+    console.log('a trc push ==== ', a)
 
-// fetch('http://localhost:3000/Duyet').then(res => res.json())
-// .then(res => {
-//     for (var i = 0; i< res.length; i++)
-//     {
-//         document.getElementById('stt').innerHTML = res[i].stt
-//         document.getElementById('NguoiThucHien').innerHTML = res[i].NguoiThucHien
-//         document.getElementById('PheDuyet').innerHTML = res[i].PheDuyet
-//         document.getElementById('NoiDung').innerHTML = res[i].NoiDung
-//     }
-// })
+    var dataAdd = {
+        NguoiTao : NguoiTao,
+        NguoiXuLy : NguoiXuLy,
+        TheoKeHoachThang : TheoKeHoachThang,
+        SoTienDuTru : SoTienDuTru,
+        TieuDe : TieuDe,
+        PhongBanLienQuan : PhongBanLienQuan,
+        ThoiGianXuLy : ThoiGianXuLy,
+        ThoiGianXuLy : ThoiGianXuLy,
+        BoPhan : BoPhan,
+        TrangThai : TrangThai,
+        Nam : Nam,
+        ThoiGianBanGiao : ThoiGianBanGiao,
+        NoiDungDeXuat : NoiDungDeXuat,
+        NgayTao : NgayTao,
+        NgayGui : NgayGui,
+        Thuoc : Thuoc,
+        NhaCungCap : NhaCungCap,
+        YKienTroLy : YKienTroLy,
+        MuaSap : MuaSap,
+        DanhSachPO : DanhSachPO
+    }
 
-fetch('http://localhost:3000/Duyet').then(res => res.json())
-.then(res => {
-    document.getElementById('stt').innerHTML = res[0].stt
-    document.getElementById('NguoiThucHien').innerHTML = res[0].NguoiThucHien
-    document.getElementById('PheDuyet').innerHTML = res[0].PheDuyet
-    document.getElementById('NoiDung').innerHTML = res[0].NoiDung
-    document.getElementById('stt1').innerHTML = res[1].stt
-    document.getElementById('NguoiThucHien1').innerHTML = res[1].NguoiThucHien
-    document.getElementById('PheDuyet1').innerHTML = res[1].PheDuyet
-    document.getElementById('NoiDung1').innerHTML = res[1].NoiDung
-})
+    if (localStorage.getItem('data') === null) {
+        localStorage.setItem('data', '[]')
+    }
+
+    var arrData = JSON.parse(localStorage.getItem('data'))
+    arrData.push(dataAdd)
+
+    localStorage.setItem('data', JSON.stringify(arrData))
+
+    // console.log(JSON.parse(localStorage.getItem('data'))[0])
+
+}
+
+
+
+for (var i = 0; i <= localStorage.length; i++) {
+    // console.log(JSON.parse(localStorage.getItem('data'))[i])
+    document.getElementById('dong1').innerHTML = JSON.parse(localStorage.getItem('data'))[i].DanhSachPO
+    document.getElementById('dong2').innerHTML = JSON.parse(localStorage.getItem('data'))[i].TieuDe
+    document.getElementById('dong3').innerHTML = JSON.parse(localStorage.getItem('data'))[i].BoPhan
+    document.getElementById('dong4').innerHTML = JSON.parse(localStorage.getItem('data'))[i].Nam
+    document.getElementById('dong5').innerHTML = JSON.parse(localStorage.getItem('data'))[i].MuaSap
+    document.getElementById('dong6').innerHTML = JSON.parse(localStorage.getItem('data'))[i].YKienTroLy
+    document.getElementById('dong7').innerHTML = JSON.parse(localStorage.getItem('data'))[i].NgayGui
+    document.getElementById('dong8').innerHTML = JSON.parse(localStorage.getItem('data'))[i].NguoiXuLy
+    document.getElementById('dong9').innerHTML = JSON.parse(localStorage.getItem('data'))[i].TrangThai
+}
